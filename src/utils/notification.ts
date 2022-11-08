2e82d74971714efafbac1b46e71f1e4dfe9acd83
lyh543 <lyh543@outlook.com>
2022-11-08 21:56:53 +0800

    fix: catch error of new Notification

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -4,7 +4,13 @@ export type NotificationOptions = globalThis.NotificationOptions;
 //  need to switch to Service Worker
 //  https://stackoverflow.com/questions/31512504/html5-notification-not-working-in-mobile-chrome
 export function sendNotification(title: string, options: NotificationOptions) {
-  return new Notification(title, options);
+  try {
+    return new Notification(title, options);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return null;
+  }
 }
 
 Notification.requestPermission();
