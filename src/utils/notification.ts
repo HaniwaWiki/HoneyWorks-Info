27ff76c165b07dc489b3e5e9251f669656a609c9
lyh543 <lyh543@outlook.com>
2022-11-26 22:46:06 +0800

    fix: use service worker to send notification

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,16 +1,11 @@
 export type NotificationOptions = globalThis.NotificationOptions;
 
-// fixme: new Notification() is not supported in Safari and Chrome Android
-//  need to switch to Service Worker
-//  https://stackoverflow.com/questions/31512504/html5-notification-not-working-in-mobile-chrome
 export function sendNotification(title: string, options: NotificationOptions) {
-  try {
-    return new Notification(title, options);
-  }
-  catch (e) {
-    console.error(e);
-    return null;
-  }
+  Notification.requestPermission((result) => {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification(title, options);
+      });
+    }
+  });
 }
-
-Notification.requestPermission();
